f8ef62871109c6e67f31fd050590a058a86ac613
Saad Bin Zahid <sbinzahid2@gmail.com>
2025-11-29 21:13:55 +0500

    4th Commit

--- Cricket Match Project in C.c
+++ Cricket Match Project in C.c
@@ -112,6 +112,152 @@ void displayremplayers(char a[][50], int b[], int k, int nonstriker)
 // inning Function
 void inning(struct team *bat, struct team *bowl, char batnames[][50], char bowlnames[][50], int batsmanruns[], int inn)
 {
+    int outplayers[10], k = 0;
+    int t2bowlerovers[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int index1, index2, strike;
+    int bowlerindex;
+    struct ball overs;
+    int f = 0;
+    // Choosing Openers
+    printf("\n\nChoose Openers (1-11):");
+    outputbat(batnames);
+    printf("\nOpener 1:");
+    scanf("%d", &index1);
+    printf("Opener 2:");
+    scanf("%d", &index2);
+    printf("Opener 1 Name: %s", batnames[index1 - 1]);
+    printf("\nOpener 2 Name: %s", batnames[index2 - 1]);
+
+    // Match Starts
+    printf("\n------Match Starts------\n------Inning 1------");
+    printf("\nNOTE: BALL STATUS: a for Normal Ball\tn for No Ball\tw for Wides\to for Out");
+    strike = index1 - 1;
+    for (int i = 0; i < 20; i++) // Changed to 20 overs
+    {
+        // Choosing Bowlers
+        printf("\n\nChoose Bowler:");
+        outputbowl(bowlnames);
+        printf("\nBowler(1-11):");
+        scanf("%d", &bowlerindex);
+        t2bowlerovers[bowlerindex - 1]++;
+        while (t2bowlerovers[bowlerindex - 1] == 4)
+        {
+            printf("\n4 overs completed for this bowler!");
+            printf("\nEnter bowler again(1-11):");
+            scanf("%d", &bowlerindex);
+        }
+
+        // Inputting Runs for Each Over
+        for (int j = 0; j < 6; j++)
+        {
+            printf("\nOn Strike: %s", batnames[strike]);
+            int outindex;
+            printf("\nEnter Over %d Ball %d runs: ", i + 1, j + 1);
+            scanf("%d", &overs.runs[i][j]);
+            printf("Enter Over %d Ball %d status: ", i + 1, j + 1);
+            scanf(" %c", &overs.status[i][j]);
+
+            // Storing Total Team Runs
+            (*bat).truns = (*bat).truns + overs.runs[i][j];
+
+            // Storing Batsman Runs
+            batsmanruns[strike] = batsmanruns[strike] + overs.runs[i][j];
+
+            // Runs Chase for 2 inning
+            if ((*bat).truns > (*bowl).truns && inn == 2)
+            {
+                f = 1;
+                break;
+            }
+
+            // All out
+            if (k == 9)
+            {
+                f = 1;
+                break;
+            }
+            if (overs.runs[i][j] == 4)
+                (*bat).boundary4++;
+            else if (overs.runs[i][j] == 6)
+                (*bat).boundary6++;
+            if (overs.runs[i][j] % 2 != 0 && overs.status[i][j] != 'w')
+            {
+                if (strike == index1 - 1)
+                    strike = index2 - 1;
+                else
+                    strike = index1 - 1;
+            }
+
+            // Handling Wide & No Balls
+            if (overs.status[i][j] == 'w' || overs.status[i][j] == 'n')
+            {
+                while (overs.status[i][j] == 'w' || overs.status[i][j] == 'n')
+                {
+                    (*bat).truns++;
+                    (*bat).extras++;
+                    if (overs.status[i][j] == 'w')
+                        (*bat).wides++;
+                    else
+                        (*bat).noballs++;
+                    printf("\nEnter Over %d Ball %d runs AGAIN: ", i + 1, j + 1);
+                    scanf("%d", &overs.runs[i][j]);
+                    printf("Enter Over %d Ball %d status AGAIN: ", i + 1, j + 1);
+                    scanf(" %c", &overs.status[i][j]);
+                    (*bat).truns = (*bat).truns + overs.runs[i][j];
+                    if (overs.status[i][j] != 'w' && overs.status[i][j] != 'n')
+                    {
+                        if (overs.runs[i][j] % 2 != 0)
+                        {
+                            if (strike == index1 - 1)
+                                strike = index2 - 1;
+                            else
+                                strike = index1 - 1;
+                        }
+                        batsmanruns[strike] = batsmanruns[strike] + overs.runs[i][j];
+                    }
+                }
+            }
+
+            // Handling Wicket
+            if (overs.status[i][j] == 'o')
+            {
+                printf("\nChoose New Batsman: ");
+                outplayers[k] = strike;
+                if (strike != index1 - 1)
+                    displayremplayers(batnames, outplayers, k, index1 - 1);
+                else
+                    displayremplayers(batnames, outplayers, k, index2 - 1);
+                k++;
+                printf("\nEnter New Batsman:");
+                if (strike == index1 - 1)
+                {
+                    scanf("%d", &index1);
+                    strike = index1 - 1;
+                }
+                else
+                {
+                    scanf("%d", &index2);
+                    strike = index2 - 1;
+                }
+            }
+        }
+        printf("\nTotal Runs after Over %d: %d", i + 1, (*bat).truns);
+
+        // Changing Strike End after Each Over
+        if (strike == index1 - 1)
+            strike = index2 - 1;
+        else
+            strike = index1 - 1;
+        if (f == 1)
+            break;
+    }
+    printf("\n\nTotal Runs scored by %s: %d", (*bat).name, (*bat).truns);
+    printf("\nExtras: %d", (*bat).extras);
+    printf("\nTotal wickets fell: %d", k + 1);
+    printf("\nTotal 4s: %d", (*bat).boundary4);
+    printf("\nTotal 6s: %d", (*bat).boundary6);
+    for (int i = 0; i < 11; i++)
+        printf("\nRuns scored by %s: %d", batnames[i], batsmanruns[i]);
 }
 
 // Finding Man Of The Match
